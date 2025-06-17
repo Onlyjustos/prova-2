7873aee82098caf0aa0824d376029f86e81f7f7b
Davi Saraiva <104288071+DaviLaw@users.noreply.github.com>
2025-06-17 17:08:43 -0300

    Update p3.c

--- arthurmiguel/p3.c
+++ arthurmiguel/p3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define dim 100
+
 int calcula_distancia(int mat[][21], char * p ){
 
     int i, x = 0, soma = 0, a ,b,z ;
@@ -7,70 +8,73 @@ int calcula_distancia(int mat[][21], char * p ){
 
     for (i = 0; p[i] != '\0'; i++) {
       
-     if (p[i] == 'A') {
-        vet[x] = 0;
-         x++;
-    } else if (p[i] == 'B') {
-        vet[x] = 1;
-         x++;
-    }else if (p[i] == 'C') {
-        vet[x] = 2;
-         x++;
-    }else if (p[i] == 'D') {
-        vet[x] = 3;
-         x++;
-    } else if (p[i] == 'E') {
-        vet[x] = 4;
-         x++;
+        if (p[i] == 'A') {
+            vet[x] = 0;
+            x++;
+        } else if (p[i] == 'B') {
+            vet[x] = 1;
+            x++;
+        } else if (p[i] == 'C') {
+            vet[x] = 2;
+            x++;
+        } else if (p[i] == 'D') {
+            vet[x] = 3;
+            x++;
+        } else if (p[i] == 'E') {
+            vet[x] = 4;
+            x++;
+        } // [1]
     }
-}
-  for  ( i = 0 ;i < x - 1; i++)
-    { 
+
+    for (i = 0; i < x - 1; i++) {
         a = vet[i];
-        b = vet[i + 1];     
+        b = vet[i + 1];
         z = mat[a][b];
-        
-        if ( mat[a][b]  ==  -1 )
-        {
+
+        if (mat[a][b] == -1) {
             return -1;
         }
-          soma = soma + mat[a][b];
-    } 
-      return soma;
 
+        soma = soma + mat[a][b];
+    }
 
+    return soma;
 }
+
 int main () {
 
-  int n, i, j;
+    int n, i, j;
 
-  scanf("%d" , &n);
+    scanf("%d", &n);
 
-  int mat[21][21];
+    int mat[21][21];
 
-   for (i = 0; i < n; i++) {
-      for (j = 0; j < n; j++) {
-          scanf("%d" , &mat[i][j]);
-      }
-   }  
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
 
     int nn;
 
-    scanf("%d" , &nn);
-    getchar();
-    
-    char vet[35];
-     
-    for (i = 0; i < nn; i++) {
-    
-     scanf("%s" , vet);  
-     int t = calcula_distancia(mat, vet);
+    scanf("%d", &nn);
+    getchar(); 
 
-     if (t == -1) {
-        printf("Caminho invalido\n");
-     } else {
-        printf("Custo: %d\n" , t);
-     }
+    char vet[35];
+
+    for (i = 0; i < nn; i++) {
+
+        scanf("%s", vet);
+        int t = calcula_distancia(mat, vet);
+
+        if (t == -1) {
+            printf("Caminho invalido\n");
+        } else {
+            printf("Custo: %d\n", t);
+        }
     }
-  return 0;
+
+    return 0;
 }
+
+// [1] Falta tratar letras fora do intervalo A-E. 
