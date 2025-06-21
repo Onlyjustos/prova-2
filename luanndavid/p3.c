27e9bc64ad9c60d70f70262cf313c4017f25ebd3
Davi Saraiva <104288071+DaviLaw@users.noreply.github.com>
2025-06-21 11:38:47 -0300

    Update p3.c

--- luanndavid/p3.c
+++ luanndavid/p3.c
@@ -1,33 +1,34 @@
- #include <stdio.h>
-
+#include <stdio.h>
+ 
 int main(){
     int n, n2;
     int l, c, i, j;
     char s1[33], s2[33], s3[33], s4[33];
     int matriz[20][20];
-
-
-    
+ 
     scanf("%d", &n);
-
+ 
     for(l = 0; l < n; l++){
         for(c = 0; c < n; c++){ 
             scanf("%d", &matriz[l][c]);
         }
     }
-
+ 
     scanf("%d", &n2);
     scanf("%s", s1);
-    printf("Custo: 28\n");
-
+    printf("Custo: 28\n"); // [1]
+ 
     scanf("%s", s2);
-    printf("Custo: 17\n");
-
+    printf("Custo: 17\n"); 
+ 
     scanf("%s", s3);
-    printf("Caminho invalido\n");
-
+    printf("Caminho invalido\n"); 
+ 
     scanf("%s", s4);
-    printf("Custo: 3\n");
-
+    printf("Custo: 3\n"); 
+ 
 return 0;
 }
+
+// Código não foi feito
+// Grave: [1] A saída  está fixa e não depende da string lida e nem da matriz. 
