e68f92d2915d7022b6128a3c423f15206ac467af
Davi Saraiva <104288071+DaviLaw@users.noreply.github.com>
2025-06-21 11:13:05 -0300

    Update p3.c

--- henriquelj/p3.c
+++ henriquelj/p3.c
@@ -9,13 +9,16 @@ int main(){
             scanf("%d", &mat[i][j]); /*lendo a matriz*/
         }
     }
-
+ 
     int a;
     char b[32], c[32], d[32], e[32];
     scanf("%d", &a);
-    scanf("%s %s %s %s", b, c, d, e);
-
-    printf("Custo: 28\nCusto: 17\nCaminho invalido\nCusto: 3\n");
+    scanf("%s %s %s %s", b, c, d, e); // [1]
+ 
+    printf("Custo: 28\nCusto: 17\nCaminho invalido\nCusto: 3\n"); // [2]
     
     return 0;
 }
+
+// Grave: [1] Os caminhos são lidos como strings, mas nenhum processamento real é feito para calcular os custos a partir da matriz.
+// Grave: [2] A saída é fixa, não depende da entrada. O programa não calcula nada. 
