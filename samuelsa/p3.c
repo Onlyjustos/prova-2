92b7a640499dfa3525129da148dea25d6ea9cc0a
Davi Saraiva <104288071+DaviLaw@users.noreply.github.com>
2025-06-21 15:15:54 -0300

    Update p3.c

--- samuelsa/p3.c
+++ samuelsa/p3.c
@@ -34,13 +34,13 @@
      
         scanf("%d", &N);
      
-        mat = (int **) malloc(N*sizeof(int*));
+        mat = (int **) malloc(N*sizeof(int*)); // [1]
         if(mat==NULL) {
             printf("ERRO AO ALOCAR A MATRIZ\n");
             return 0;
         }
         for(i=0;i<N;i++) {
-            mat[i] = (int *) malloc(N*sizeof(int));
+            mat[i] = (int *) malloc(N*sizeof(int)); // [1]
         }
         for(l=0;l<N;l++) {
             for(c=0;c<N;c++) {
@@ -49,7 +49,7 @@
         }
      
         scanf("%d%*c", &n_testes);
-
+ 
         for(j=0;j<n_testes;j++) {
              //Alocaçao da string
             string = (char *) malloc((32+1)*sizeof(char));
@@ -74,3 +74,5 @@
         free(mat);
         return 0;
     }
+
+// [1] O código está correto, todavia, está dando runtime possivelmente porque assume que a entrada só irá conter letras entre 'A' e 'A' + N - 1.
