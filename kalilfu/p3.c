387e98537b093e67c6c90880084d93478dd27fd4
Davi Saraiva <104288071+DaviLaw@users.noreply.github.com>
2025-06-21 11:36:34 -0300

    Update p3.c

--- kalilfu/p3.c
+++ kalilfu/p3.c
@@ -2,6 +2,9 @@
  
 int main(){
   
-  printf("Custo: 28\nCusto: 17\nCaminho invalido\nCusto: 3\n");
+  printf("Custo: 28\nCusto: 17\nCaminho invalido\nCusto: 3\n"); // [1]
     return 0;
 }
+
+// Código não foi feito
+// Grave: [1] As saídas são fixas e não dependem de nenhuma entrada ou cálculo real. 
